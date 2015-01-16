4d157d42cfc003362a6509c1370add76392b132a
leeikje@gmail.com <leeikje@gmail.com>
2015-01-16 17:42:01 +0900

    NQUEEN 문제 풀이완료.

--- tutorial/NQUEEN.py
+++ tutorial/NQUEEN.py
@@ -1,43 +1,32 @@
 import sys
 
-ca = []
-cb = []
-cc = []
+ca = [0 for i in xrange(12) ]
+cb = [0 for i in xrange(24) ]
+cc = [0 for i in xrange(24) ]
 
 def nqueen(line, n):
+    if line == n:
+        return 1
+
+    sum = 0
     for i in xrange(n):
-        if ca[i] == 0 and cb[line+i] == 0 and cc[n+line-i]
+        if ca[i] == 0 and cb[line+i] == 0 and cc[line-i+n] == 0:
+            ca[i] = 1
+            cb[line+i] = 1
+            cc[line-i+n] = 1
+            sum += nqueen( line+1, n )
+            ca[i] = 0
+            cb[line+i] = 0
+            cc[line-i+n] = 0
+
+    return sum
 
 rl = lambda: sys.stdin.readline()
 n = int(rl())
 
-for i in range(n):
+for i in xrange(n):
     n = int(rl().strip())
 
+    print( nqueen(0,n) )
 
 
-    priority = {}
-    for i in range(4):
-        priority[ps[i]] = i
-        
-    st = []
-    result = ""
-    tmp = ""
-    for c in seq:
-        if c in "({[<":
-            st.append(c)
-        else:            
-            open = st.pop()
-            if c != open:
-                if priority[open] < priority[opens[c]]:
-                    tmp = open + tmp + closes[open]
-                else:
-                    tmp = opens[c] + tmp + c
-            else:
-                tmp = open + tmp + c
-            if len(st) <= 0:
-                result = result + tmp
-                tmp = ""
-    result = result + tmp
-
-    print(result)
